dbdb1f30175508371840b725006c79aec24564cf
roypeled <peled.roy@gmail.com>
2023-06-08 12:31:08 +0300

    first

--- apps/foo/jest.config.ts
+++ apps/foo/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'watch-test',
+  displayName: 'foo',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/watch-test',
+  coverageDirectory: '../../coverage/apps/foo',
 };
